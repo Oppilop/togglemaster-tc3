d8d0b9e09f4223850540ffe4935c28097a49b177
Oppilop <nicolepolippo18@gmail.com>
2026-05-03 20:46:00 -0300

    chore: yml unificadoi

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "togglemaster-s3"
-    key    = "state/terraform.tfstate"
-    region = "us-east-1"
-    # use_lockfile = true (Se estiver usando Terraform 1.10+)
-  }
-}
